5ef2702853d103e3f9df9e98d227326d499f47a0
Lee Hong Joo <101697954+Archel0906@users.noreply.github.com>
2025-10-30 14:41:06 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,13 +1,11 @@
-for x in range(1, 101):
-    # 2의 배수이면서 5의 배수인 경우
-    if x % 2 == 0 and x % 5 == 0:
-        print(f"{x}는 2와 5의 배수입니다.")
-    # 2의 배수인 경우
-    elif x % 2 == 0:
-        print(f"{x}는 2의 배수입니다.")
-    # 5의 배수인 경우
-    elif x % 5 == 0:
-        print(f"{x}는 5의 배수입니다.")
-    # 그 외
+i = 1
+while i < 101:
+    if i % 2 == 0 and i % 5 == 0:
+        print("2와 5의 배수입니다.")
+    elif i % 2 == 0:
+        print("2의 배수입니다.")
+    elif i % 5 == 0:
+        print("5의 배수입니다.")
     else:
-        print(x)
+        print(str(i))
+    i += 1
