0da7e07f38bba6a8e28d55ecc76b859ec6c0dbfd
Lee Hong Joo <101697954+Archel0906@users.noreply.github.com>
2025-10-30 15:24:57 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,1 +1,16 @@
-.
+i = 100
+while i > 0:
+    j = i // 10
+    k = i % 10
+    if (j % 3 == 0 and j != 0) and (k % 3 == 0 and k != 0):
+        print("짝짝")
+    elif (j % 3 == 0 and j != 0) or (k % 3 == 0 and k != 0):
+        print("짝")
+    else:
+        print(str(i))
+    i -= 1
+
+for i in range(100, 0, -1):
+    s = str(i)
+    clap = s.count('3') + s.count('6') + s.count('9')
+    print('짝'*clap if clap else i)
